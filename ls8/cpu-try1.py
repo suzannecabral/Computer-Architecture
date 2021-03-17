65f07810cdbe443430892533c707197a7237b0cd
Suzanne Cabral <suzannecabral@live.com>
2021-03-16 19:05:03 -0700

    Redo, test print works os far. Remember to run ls8.py not cpu.py

--- ls8/cpu-try1.py
+++ ls8/cpu-try1.py
@@ -7,44 +7,61 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        # add self.ram
         self.ram = [0] * 256
+        # 256 slots, leave one out so it can move =255
+
+        # later will remove op codes, hardcoded for now
+        # self.op = op
+
+        # registers are lists 8 bytes long
         self.reg = [0] * 8
-        
+
+        # program counter
         self.pc = 0
 
+    # read from ram
     def ram_read(self, address):
-        pass
+        return self.ram[address]
 
-    def ram_write(self, address, value):
-        pass
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
-
+        
         address = 0
 
         # For now, we've just hardcoded a program:
 
+        # op codes - hard coded
         program = [
             # From print8.ls8
             0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
+            0b00000000, # NOP - no operation
+            0b00001000, # not listed?
             0b01000111, # PRN R0
-            0b00000000,
+            0b00000000, # NOP - no operation
             0b00000001, # HLT
         ]
 
         for instruction in program:
+            # creating a ram address for each instruction
             self.ram[address] = instruction
             address += 1
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+        # alu is for mathematical operations
+        # like a calculator
+
+        # has op
+        # has registers
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -71,7 +88,5 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        ir = self.reg[self.pc]
-        print("self.reg",self.reg)
-        print("ir",ir)
-        print("Test run complete")
+        instruction_register = 0
+        print("Test complete")
